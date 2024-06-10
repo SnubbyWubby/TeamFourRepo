57ca53462c12a294fa9e55d9dfec1614a2c05153
Dylan Wagner <djwagner@student.fullsail.edu>
2024-06-09 22:24:27 -0400

    Created playerController, setup basic movement variables and a movement function.

---
+++ Team Four FPS/Assets/Scripts/playerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] int speed;
+
+    Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void movement()
+    {
+        // Get input from player, create vector, then move player.
+        moveDirection = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDirection * speed * Time.deltaTime);
+    }
+}
