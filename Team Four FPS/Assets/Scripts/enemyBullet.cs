7acd1de24cd8497f09dedc38bef3f54fffbe627c
Jeffrey Nova <141005765+JeffNova93@users.noreply.github.com>
2024-06-12 06:07:23 -0500

    Added An Enemy Bullet Class To Our Enemies

---
+++ Team Four FPS/Assets/Scripts/enemyBullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class enemyBullet : MonoBehaviour 
+{
+	[SerializeField] Rigidbody enyBullet;
+
+	[SerializeField] int dmgBullet;
+	[SerializeField] int spdBullet;
+	[SerializeField] int dstBullet; 
+
+	// Start Is Called Before The First Frame Update 
+	void Start()
+	{
+		enyBullet.velocity = transform.forward * spdBullet;
+
+		Destroy(gameObject, dstBullet); 
+	}
+
+	private void OnTriggerEnter(Collider other) 
+	{
+		if (other.isTrigger) { return; }
+
+		IDamage damage = other.GetComponent<IDamage>();
+
+		if (damage != null) 
+		{
+			damage.takeDamage(dmgBullet); 
+		}
+
+		Destroy(gameObject);
+	}
+}
