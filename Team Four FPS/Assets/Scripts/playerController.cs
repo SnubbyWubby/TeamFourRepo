6ae265e584b36a2f941ed2b57e006df235829fa6
Dylan Wagner <djwagner@student.fullsail.edu>
2024-06-13 00:34:14 -0400

    Jump mechanic. Enemy updates. UI prefab update.
    Added code for player jump mechanic.
    Updated enemy 2 and 3's hp.
    Updated UI prefab.

--- Team Four FPS/Assets/Scripts/playerController.cs
+++ Team Four FPS/Assets/Scripts/playerController.cs
@@ -10,15 +10,20 @@ public class playerController : MonoBehaviour
     [SerializeField] int shootDamage;
     [SerializeField] int shootRate;
     [SerializeField] int shootDistance;
+    [SerializeField] int jumpMax;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int gravity;
 
     [SerializeField] float speed;
     [SerializeField] float sprintModifier;
 
     Vector3 moveDirection;
+    Vector3 playerVelocity;
 
     bool isShooting;
 
     int originalHP;
+    int jumpCount;
 
     // Start is called before the first frame update
     void Start()
@@ -39,9 +44,25 @@ public class playerController : MonoBehaviour
 
     void movement()
     {
+        // Check if player is touching the ground
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVelocity = Vector3.zero;
+        }
+
         // Get input from player, create vector, then move player.
         moveDirection = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
         controller.Move(moveDirection * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVelocity.y = jumpSpeed;
+        }
+
+        playerVelocity.y -= gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 
     void sprint()
