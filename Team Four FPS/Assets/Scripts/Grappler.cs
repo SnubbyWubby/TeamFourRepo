9a429f8a2a6d18fa2a1a474c07a6b787c4ef2e80
William Roland jr. <143528569+SnubbyWubby@users.noreply.github.com>
2024-06-25 22:06:40 -0400

    Update to Grappler Test

--- Team Four FPS/Assets/Scripts/Grappler.cs
+++ Team Four FPS/Assets/Scripts/Grappler.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Grappler : MonoBehaviour
 {
-    [SerializeField] int gappleTime;
+    [SerializeField] float gappleTime;
+
     [SerializeField] int maxGrappleDistance;
     bool isGrappling = false;
+
+
+
+    CharacterController Controller;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -25,13 +31,17 @@ public class Grappler : MonoBehaviour
     IEnumerator Grapple()
     {
         RaycastHit hit;
-
+        isGrappling = true;
         // Use raycast and get gameobject that is hit
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxGrappleDistance))
         {
-
-            //hit.transform
+            Debug.Log(hit.point);
+            if (hit.transform != transform)
+            {
+                transform.position = Vector3.Lerp(transform.position, hit.point, gappleTime);
+            }
         }
         yield return new WaitForSeconds(gappleTime);
+        isGrappling = false;
     }
 }
