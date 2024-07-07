d7516b9207d7b4c081410865cd4598bd900d7b6e
Blake Farrar <143566617+Rainsalot@users.noreply.github.com>
2024-07-07 11:43:01 -0700

    Implemented Death Camera
    implemented logic to lerp our camera backwards to view the player on death, stagger our fail menu to view our player (eventually) ragdoll, cut the players ability to move during the transition. +10 bools

--- Team Four FPS/Assets/Scripts/playerCameraController.cs
+++ Team Four FPS/Assets/Scripts/playerCameraController.cs
@@ -17,35 +17,41 @@ public class playerCameraController : MonoBehaviour
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        GameManager.Instance.camEnd = new Vector3(Camera.main.transform.localPosition.x,
+                                                  Camera.main.transform.localPosition.y + 5,
+                                                  Camera.main.transform.localPosition.z - 7);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get Input 
-
-        float xMouse = Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-        float yMouse = Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
-
-        if (cameraInvertY)
+        if (!GameManager.Instance.stopCameraRotation)
         {
-            cameraRotationX += yMouse;
+            float xMouse = Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+            float yMouse = Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+
+            if (cameraInvertY)
+            {
+                cameraRotationX += yMouse;
+            }
+            else
+            {
+                cameraRotationX -= yMouse;
+            }
+
+            // Clamp The cameraRotationX On The X-Axis 
+
+            cameraRotationX = Mathf.Clamp(cameraRotationX, cameraLockVertMin, cameraLockVertMax);
+
+            // Rotate The Camera On The X-Axis 
+
+            transform.localRotation = Quaternion.Euler(cameraRotationX, 0, 0);
+
+            // Rotate The Player On The Y-Axis 
+
+            transform.parent.Rotate(Vector3.up * xMouse);
         }
-        else
-        {
-            cameraRotationX -= yMouse;
-        }
-
-        // Clamp The cameraRotationX On The X-Axis 
-
-        cameraRotationX = Mathf.Clamp(cameraRotationX, cameraLockVertMin, cameraLockVertMax);
-
-        // Rotate The Camera On The X-Axis 
-
-        transform.localRotation = Quaternion.Euler(cameraRotationX, 0, 0);
-
-        // Rotate The Player On The Y-Axis 
-
-        transform.parent.Rotate(Vector3.up * xMouse);
+        
     }
 }
