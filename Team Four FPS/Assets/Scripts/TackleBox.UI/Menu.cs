29d42a29962155e0f6c973947280cc3fb3fbc411
Jeffrey Nova <141005765+JeffNova93@users.noreply.github.com>
2024-07-10 08:14:39 -0500

    Updated The UI & Player Prefabs For Mini-Map & Added A Basic Mini-Map Camera Script

--- Team Four FPS/Assets/Scripts/TackleBox.UI/Menu.cs
+++ Team Four FPS/Assets/Scripts/TackleBox.UI/Menu.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 namespace TackleBox.UI
 {
     [CreateAssetMenu(fileName = "Menu", menuName = "TackleBox.UI/Menu", order = 1)]
+
     public class Menu : ScriptableObject
     {
         [SerializeField] private GameObject _menu;
-
-
     }
 }
