cc7d2119951516a79eadcf24195fea49405eb461
Jeffrey Nova <141005765+JeffNova93@users.noreply.github.com>
2024-07-12 09:44:26 -0500

    Updated The Main Menu (Prefab & Script) & UI Prefab

--- Team Four FPS/Assets/Scripts/TackleBox.UI/MainMenu.cs
+++ Team Four FPS/Assets/Scripts/TackleBox.UI/MainMenu.cs
@@ -1,26 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 using UnityEngine.SceneManagement;
 
 namespace TackleBox.UI
 {
     public class MainMenu : MonoBehaviour
     {
+        [Header("<=====VIDEO_GAME_MENUS=====>")]
+
         [SerializeField] string PlayScene;
+        public string playNewGame;
+        private string loadCurrGame;
+
+        [SerializeField] private GameObject noFileFound = null; 
 
         public void play()
         {
             if (!string.IsNullOrEmpty(PlayScene))
                 SceneManager.LoadScene(PlayScene);
-
-
         }
 
-        //public void options()
-        //{
-        //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        //}
+        public void LoadGame()  
+        {
+            if (PlayerPrefs.HasKey("LevelSaved"))
+            {
+                loadCurrGame = PlayerPrefs.GetString("LevelSaved");
+
+                SceneManager.LoadScene(loadCurrGame); 
+            }
+            else
+            {
+                noFileFound.SetActive(false); 
+            }
+        }
+
+        public void options()
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 
         public void quit()
         {
