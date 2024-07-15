fae54e71cd86a1658785b642eff513ab0697f82a
Jeffrey Nova <141005765+JeffNova93@users.noreply.github.com>
2024-07-15 04:45:03 -0500

    Updated The UI (+UI W/HUD) Prefabs & Ammo Box (Prefab & Script)

--- Team Four FPS/Assets/Scripts/ammoPickup.cs
+++ Team Four FPS/Assets/Scripts/ammoPickup.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using TackleBox;
+using TackleBox.Audio; 
 using TackleBox.Guns;
 using UnityEngine;
 
 public class ammoPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("<=====AMMO_PICK_UP_AUDIO=====>")]
+
+    [SerializeField] AudioSource boxAudio;
+
+    // Awake is called before the first frame update
+    void Awake() 
     {
-        
+        playerController playerAmmo = GameManager.Instance.PlayerScript;
+        foreach (TackleBox.Guns.gunStats gun in playerAmmo.gunList)
+        {
+            AudioManager soundManager = AudioManager.Instance;
+            Audio AmmoBox = soundManager.GetSoundByID("AmmoBox");
+            AmmoBox.PlayOneShot(boxAudio);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -24,9 +30,10 @@ public class ammoPickup : MonoBehaviour
             playerController playerAmmo = GameManager.Instance.PlayerScript;
             foreach(TackleBox.Guns.gunStats gun in playerAmmo.gunList)
             {
+                boxAudio.Stop(); 
                 gun.ammoCurr = gun.ammoMax;
             }
-            
+
             playerAmmo.grenadeCount = 5;
             GameManager.Instance.grenadeCount = 5;
             playerAmmo.updatePlayerUI();
